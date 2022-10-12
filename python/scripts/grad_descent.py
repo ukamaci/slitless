fix bug in GD, add psnr calculation

python/scripts/grad_descent.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 from torch import optim
 from slitless.data_loader import BasicDataset
 from slitless.forward import Source, Imager, forward_op_torch
-from slitless.measure import cycle_loss, compare_ssim, tv_loss, grad_res_loss
+from slitless.measure import cycle_loss, compare_ssim, compare_psnr, tv_loss, grad_res_loss
 from tqdm.auto import tqdm
 
 dataset_path = glob.glob('/home/kamo/resources/slitless/data/datasets/dset3*')[0]
@@ -33,6 +33,7 @@ USE_OPTIMIZER = True
 USE_TV_LOSS = True
 GRAD_RES_LOSS = False
 DATA_FIDELITY = 'L2' # 'L1' or 'L2'
+maxiters = 20000
 mu_vel = 1e1 # GD step size for vel
 mu_width = 1e1 # GD step size for width
 lam_v = 1e-2 # TV norm regularization parameter for velocity
@@ -40,7 +41,6 @@ lam_w = 1e-1 # TV norm regularization parameter for width
 regparamdec = 6 # TV regu. param. exp.tial decay (set to 0 for no decay)
 beta = 10**(-regparamdec/maxiters) # TV reg. param. multiplier
 LR = 1e-2
-maxiters = 20000
 losses = []
 diffs_vel = []
 diffs_width = []
@@ -52,12 +52,13 @@ xh_int = x[0,0]
 xh_vel = torch.zeros(
     (meas.shape[1],meas.shape[2]), device=device, dtype=torch.float, requires_grad=True)
 # xh_vel = x[0,1]
-xh_width = 0.25*torch.ones((meas.shape[1],meas.shape[2]), device=device, dtype=torch.float)
+xh_width = 1*torch.ones((meas.shape[1],meas.shape[2]), device=device, dtype=torch.float)
 xh_width = xh_width.requires_grad_()
 # xh_width = x[0,2]
 optimizer = optim.Adam([xh_vel, xh_width], lr=LR)
 # optimizer = optim.SGD([xh_vel, xh_width], lr=LR, momentum=0.5)
 xwidths = []
+xvels = []
 
 for i in tqdm(range(maxiters)):
     if USE_OPTIMIZER:
@@ -99,6 +100,7 @@ for i in tqdm(range(maxiters)):
     diffs_width.append(diff_width.detach().cpu().numpy())
     if i%1000==0:
         xwidths.append(xh_width.detach().cpu().numpy())
+        xvels.append(xh_vel.detach().cpu().numpy())
 
 # %% plots
 plt.figure()
@@ -129,8 +131,10 @@ sr2 = Source(
     pix=True
 )
 ssims = compare_ssim(truth=sr.param3d, estimate=sr2.param3d)
+# assume maxval for (int,vel,width)=(1,2,2.5)
+psnrs = compare_psnr(truth=sr.param3d, estimate=sr2.param3d, maxval=(1,2,2.5))
 
 sr.plot('Original')
 plt.tight_layout()
-sr2.plot('Estimated', ssims=ssims)
+sr2.plot('Estimated', ssims=ssims, psnrs=psnrs)
 plt.tight_layout()