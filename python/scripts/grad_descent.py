1c3f3f17db3049cfa43bf3d8b6449929bd163d2c
ukamaci <kamaci.ulas@gmail.com>
2022-10-12 10:56:25 -0500

    add TV loss, ADAM, TV scheduler to GD

--- python/scripts/grad_descent.py
+++ python/scripts/grad_descent.py
@@ -4,9 +4,10 @@ import torch, glob
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
+from torch import optim
 from slitless.data_loader import BasicDataset
 from slitless.forward import Source, Imager, forward_op_torch
-from slitless.measure import cycle_loss
+from slitless.measure import cycle_loss, compare_ssim, tv_loss, grad_res_loss
 from tqdm.auto import tqdm
 
 dataset_path = glob.glob('/home/kamo/resources/slitless/data/datasets/dset3*')[0]
@@ -28,9 +29,18 @@ pixelated = True
 imgr = Imager(pixelated=pixelated)
 imgr.get_measurements(sr)
 
-mu_vel = 1e3 # GD step size for vel
-mu_width = 2e3 # GD step size for width
-maxiters = 10000
+USE_OPTIMIZER = True
+USE_TV_LOSS = True
+GRAD_RES_LOSS = False
+DATA_FIDELITY = 'L2' # 'L1' or 'L2'
+mu_vel = 1e1 # GD step size for vel
+mu_width = 1e1 # GD step size for width
+lam_v = 1e-2 # TV norm regularization parameter for velocity
+lam_w = 1e-1 # TV norm regularization parameter for width
+regparamdec = 6 # TV regu. param. exp.tial decay (set to 0 for no decay)
+beta = 10**(-regparamdec/maxiters) # TV reg. param. multiplier
+LR = 1e-2
+maxiters = 20000
 losses = []
 diffs_vel = []
 diffs_width = []
@@ -42,23 +52,40 @@ xh_int = x[0,0]
 xh_vel = torch.zeros(
     (meas.shape[1],meas.shape[2]), device=device, dtype=torch.float, requires_grad=True)
 # xh_vel = x[0,1]
-xh_width = 0.75*torch.ones((meas.shape[1],meas.shape[2]), device=device, dtype=torch.float)
+xh_width = 0.25*torch.ones((meas.shape[1],meas.shape[2]), device=device, dtype=torch.float)
 xh_width = xh_width.requires_grad_()
 # xh_width = x[0,2]
+optimizer = optim.Adam([xh_vel, xh_width], lr=LR)
+# optimizer = optim.SGD([xh_vel, xh_width], lr=LR, momentum=0.5)
+xwidths = []
 
 for i in tqdm(range(maxiters)):
-    loss = torch.mean((meas-forward_op_torch(xh_int,xh_vel,xh_width, pixelated=pixelated))**2)
+    if USE_OPTIMIZER:
+        optimizer.zero_grad()
+    # compute the residual
+    res = meas-forward_op_torch(xh_int,xh_vel,xh_width, pixelated=pixelated)
+    if DATA_FIDELITY == 'L1':
+        loss = torch.mean(abs(res))
+    elif DATA_FIDELITY == 'L2': 
+        loss = torch.mean(res**2)
+    if GRAD_RES_LOSS:
+        loss += grad_res_loss(res, loss=DATA_FIDELITY)
+    if USE_TV_LOSS:
+        loss += beta**i*(lam_v*tv_loss(xh_vel) + lam_w*tv_loss(xh_width))
     loss.backward()
 
-    with torch.no_grad():
-        xh_vel -= mu_vel * xh_vel.grad
-        xh_vel.grad.zero_()
-        xh_width -= mu_width * xh_width.grad
-        xh_width.grad.zero_()
+    if USE_OPTIMIZER:
+        optimizer.step()
+    else:
+        with torch.no_grad():
+            xh_vel -= mu_vel * xh_vel.grad
+            xh_vel.grad.zero_()
+            xh_width -= mu_width * xh_width.grad
+            xh_width.grad.zero_()
 
-        losses.append(loss.detach().cpu().numpy())
-        diff_vel = torch.sum((x[0,1]-xh_vel)**2)/torch.sum(x[0,1]**2)
-        diffs_vel.append(diff_vel.detach().cpu().numpy())
+    losses.append(loss.detach().cpu().numpy())
+    diff_vel = torch.sum((x[0,1]-xh_vel)**2)/torch.sum(x[0,1]**2)
+    diffs_vel.append(diff_vel.detach().cpu().numpy())
 
     # loss = torch.mean((meas-forward_op_torch(xh_int,xh_vel.detach(),xh_width, pixelated=pixelated))**2)
     # loss.backward()
@@ -68,9 +95,12 @@ for i in tqdm(range(maxiters)):
         # xh_width.grad.zero_()
 
     #     losses.append(loss.detach().cpu().numpy())
-        diff_width = torch.sum((x[0,2]-xh_width)**2)/torch.sum(x[0,2]**2)
-        diffs_width.append(diff_width.detach().cpu().numpy())
+    diff_width = torch.sum((x[0,2]-xh_width)**2)/torch.sum(x[0,2]**2)
+    diffs_width.append(diff_width.detach().cpu().numpy())
+    if i%1000==0:
+        xwidths.append(xh_width.detach().cpu().numpy())
 
+# %% plots
 plt.figure()
 plt.plot(losses)
 plt.title('Loss vs Iter')
@@ -95,10 +125,12 @@ plt.show()
 sr2 = Source(
     inten=xh_int.detach().cpu().numpy(),
     vel=xh_vel.detach().cpu().numpy(),
-    width=xh_width.detach().cpu().numpy()
+    width=xh_width.detach().cpu().numpy(),
+    pix=True
 )
+ssims = compare_ssim(truth=sr.param3d, estimate=sr2.param3d)
 
 sr.plot('Original')
 plt.tight_layout()
-sr2.plot('Estimated')
+sr2.plot('Estimated', ssims=ssims)
 plt.tight_layout()
