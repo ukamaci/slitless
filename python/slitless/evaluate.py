fix the rmse blowup issue (minmax normalization)

python/slitless/evaluate.py
@@ -195,7 +195,7 @@ def plot_val_stats(net, valloader, savedir):
             outputs = net(inputs)
             outputs = np.array(outputs.cpu())
             ssim0 = compare_ssim(truth=y1, estimate=outputs)
-            rmse0 = nrmse(truth=y1, estimate=outputs, normalization='minmax')
+            rmse0 = nrmse(truth=y1, estimate=outputs, normalization=None)
             ssims.extend(ssim0.squeeze())
             rmses.extend(rmse0.squeeze())
             if i*valloader.batch_size<10000:

python/slitless/measure.py
@@ -10,7 +10,7 @@ def compare_ssim(*, truth, estimate):
     return skimage_ssim(estimate, truth, data_range=np.max(truth) - np.min(truth))
 
 @_vectorize(signature='(a,b),(a,b)->()', included=['truth', 'estimate'])
-def nrmse(*, truth, estimate, normalization='minmax'):
+def nrmse(*, truth, estimate, normalization=None):
     if normalization=='sigma':
         norm = np.std(truth)
     elif normalization=='minmax':