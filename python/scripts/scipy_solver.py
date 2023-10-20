07930b16b893ecd48afc3b61649acabe2cf35625
ukamaci <kamaci.ulas@gmail.com>
2023-10-20 16:00:26 -0500

    horizontal dispersion by np.rot90 in scipy_solver

--- python/scripts/scipy_solver.py
+++ python/scripts/scipy_solver.py
@@ -15,11 +15,12 @@ spectral_orders=[0,-1,1]
 aa, bb = inten.shape
 
 path_data = '/home/kamo/resources/slitless/data/eis_data/datasets/dset_v1/meta/selected_scans_train/'
-date='20071211_002416'
+date='20071211_002416' #APJ2019 image
+# Rotate the params so that the effective dispersion direction is horizontal
 sr = Source(
-    inten=np.load(path_data+'int_{}.npy'.format(date))[149:169, 182:202],
-    vel=np.load(path_data+'vel_{}.npy'.format(date))[149:169, 182:202],
-    width=np.load(path_data+'width_{}.npy'.format(date))[149:169, 182:202],
+    inten=np.rot90(np.load(path_data+'int_{}.npy'.format(date))[149:169, 182:202]),
+    vel=np.rot90(np.load(path_data+'vel_{}.npy'.format(date))[149:169, 182:202]),
+    width=np.rot90(np.load(path_data+'width_{}.npy'.format(date))[149:169, 182:202]),
     pix=False
 )
 imgr = Imager(pixelated=False)
@@ -28,9 +29,9 @@ inten, vel, width = imgr.srpix.param3d
 # inten, vel, width = imgr.srpix.param3d.transpose(0,2,1)
 
 # meas = forward_op(inten, vel, width, pixelated=False)
-meas = imgr.get_measurements(max_count=50**2/0.9, model='poisson')
+meas = imgr.get_measurements(max_count=50**2/0.9, model='poisson',no_noise=False)
 
-def obj_ls(x, meas=None, apj_regu=False, a1=1e2, a2=1e2):
+def obj_ls(x, meas=None, apj_regu=False, a1=1e1, a2=1e1):
     aa, bb = meas.shape[1:]
     intensity, doppler, linewidth = np.reshape(x, (3,aa,bb))
     diff = forward_op(intensity, doppler, linewidth, pixelated=False) - meas
@@ -50,14 +51,14 @@ fig, ax = plt.subplots()
 for i in tqdm(range(bb)):
     x0 = np.stack( ( meas[0,:,i], vel0[:,i], width0[:,i] ), axis=0 ).flatten()
     # recon = minimize(obj_ls, x0, args=(meas,), method='Nelder-Mead',
-    recon = minimize(obj_ls, x0, args=(meas[:,:,[i]], True, 1e3, 1e3), method='L-BFGS-B',
+    recon = minimize(obj_ls, x0, args=(meas[:,:,[i]], True, 5e2, 5e2), method='L-BFGS-B',
         options={'disp':False, 'maxiter':10000, 'adaptive':True})
     rec[:,:,i] = recon.x.reshape(3,aa)
-    ax.imshow(rec[1], cmap='seismic')
+    ax.imshow(np.rot90(rec[1],k=3), cmap='seismic')
     plt.pause(0.1)
     plt.show()
 
-
+rec = np.rot90(rec, k=3, axes=(1,2))
 # inten, vel, width = imgr.srpix.param3d
 # rec = rec.transpose(0,2,1)
 # %% plot
@@ -68,5 +69,7 @@ sr_r = Source(
     pix=True
 )
 
-imgr.srpix.plot('Orig')
+sr_o = Source(param3d=np.rot90(imgr.srpix.param3d,k=3,axes=(1,2)), pix=True)
+
+sr_o.plot('Orig')
 sr_r.plot('Recon')
